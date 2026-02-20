cb2e95cab5d0ce453389b2cc7f2f70f81bcd2e62
Dmitrii Makarov <megabrain87@gmail.com>
2026-02-20 22:49:18 +0100

    implementation guess game

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -1,3 +1,35 @@
+use rand::Rng;
+use std::{cmp::Ordering, io};
+
 fn main() {
-    println!("Hello, world!");
+    println!("Game is started!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u8 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                if guess.trim() == "quite" {
+                    break;
+                }
+                continue;
+            }
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too Small!"),
+            Ordering::Greater => println!("Too Big!"),
+            Ordering::Equal => {
+                println!("Bingo!");
+                break;
+            }
+        }
+    }
 }

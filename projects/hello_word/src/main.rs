6577be2404f339a2f50220e652de56fd10ffaa93
Dmitrii Makarov <megabrain87@gmail.com>
2026-02-20 20:58:33 +0100

    updated basic project

--- projects/hello_word/src/main.rs
+++ projects/hello_word/src/main.rs
@@ -1,3 +1,28 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Hello World Next Level");
+
+    let tuple: (u8, f32, bool) = (1, 1.1, true);
+    println!("{1} {2} {0}", tuple.0, tuple.1, tuple.2);
+
+    let nums: [u8; 5] = [1, 2, 3, 4, 5];
+    println!("{:?}", nums);
+    println!("{:#?}", nums);
+
+    let nums = [0; 5];
+
+    println!("{}", nums[1]);
+
+    // let x = y = 6;
+
+    // println!("{x}");
+
+    fn getFive() -> u8 {
+        22;
+        255;
+        33
+    }
+
+    println!("{}", getFive());
+
+    let x = 5;
 }

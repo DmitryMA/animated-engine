6463cbea046f2660cc8d78f2cc84e600990cab09
Dmitrii Makarov <megabrain87@gmail.com>
2026-02-26 21:54:42 +0100

    vector exercises

--- projects/collections/src/main.rs
+++ projects/collections/src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
-    println!("Hello, world!");
+    let mut v = vec![1, 2, 3];
+    v.push(12);
+    v.push(22);
+    v.push(42);
+
+    println!("{:#?}", v);
+
+    // let item_1 = &v[v.len()];
+    let idx = v.len();
+    let item_2 = v.get(idx);
+    match item_2 {
+        Some(value) => println!("{value}"),
+        None => println!("There is no element with such Index: {idx}"),
+    }
+
+    // println!("1: {}", item_1);
+    // println!("2: {:#?}", item_2);
+
+    for item in &mut v {
+        *item += 20;
+        println!("{item}");
+    }
 }
